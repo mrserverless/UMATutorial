240ad21e284596868df96f4c8eda5db81805d82c
Yun Zhi Lin <yun@yunspace.com>
2015-10-13 22:49:32 +1100

    tutorial 3

---
+++ Assets/UMAMaker.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UMA;
+
+public class UMAMaker : MonoBehaviour {
+	
+	public UMAGeneratorBase generator;
+	public SlotLibrary slotLibrary;
+	public OverlayLibrary overlayLibrary;
+	public RaceLibrary raceLibrary;
+	
+	private UMADynamicAvatar umaDynamicAvatar; // needed to display uma character
+	private UMAData umaData; // used by dynamic avatar
+	private UMADnaHumanoid umaDna; 
+	private UMADnaTutorial umaTutorialDna; // optional make your own dna
+	
+	private int numberOfSlots = 20; // 
+}
