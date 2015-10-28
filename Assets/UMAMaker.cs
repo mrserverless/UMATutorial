96e63e5531a52ac92204fdfd648b45960eb01e8a
Yun Zhi Lin <yun@yunspace.com>
2015-10-29 00:33:11 +1100

    lesson 5

--- Assets/UMAMaker.cs
+++ Assets/UMAMaker.cs
@@ -14,5 +14,50 @@ public class UMAMaker : MonoBehaviour {
 	private UMADnaHumanoid umaDna; 
 	private UMADnaTutorial umaTutorialDna; // optional make your own dna
 	
-	private int numberOfSlots = 20; // 
+	private int numberOfSlots = 20; // slots to be added to UMA
+	
+	void Start() {
+		GenerateUMA();	
+	}
+	
+	void GenerateUMA () {
+		
+		GameObject go = new GameObject("MyUMA");
+		umaDynamicAvatar = go.AddComponent<UMADynamicAvatar>();
+		
+		// initialize avatar and grab reference to data
+		umaDynamicAvatar.Initialize();
+		umaData = umaDynamicAvatar.umaData;
+		
+		// assign generator
+		umaDynamicAvatar.umaGenerator = generator;
+		umaData.umaGenerator = generator;
+		
+		// setup slot array
+		umaData.umaRecipe.slotDataList = new SlotData[numberOfSlots];
+		
+		umaDna = new UMADnaHumanoid();
+		umaTutorialDna = new UMADnaTutorial();
+		umaData.umaRecipe.AddDna(umaDna);
+		umaData.umaRecipe.AddDna(umaTutorialDna);
+		
+		CreateMale();
+		
+		// Generate UMA
+		umaDynamicAvatar.UpdateNewRace();
+		
+	}
+	
+	void CreateMale() {
+		// grab reference to receipe
+		var umaRecipe = umaDynamicAvatar.umaData.umaRecipe;
+		
+		umaRecipe.SetRace(raceLibrary.GetRace("HumanMale"));
+			
+		SlotData maleFace = slotLibrary.InstantiateSlot("MaleFace");
+		maleFace.AddOverlay(overlayLibrary.InstantiateOverlay("MaleHead02"));
+		umaData.umaRecipe.slotDataList[0] = maleFace;
+		
+		
+	}
 }
