bd331c6bf15e2c4ed8eff231c9b2600d1545d8c4
Yun Zhi Lin <yun@yunspace.com>
2015-11-09 17:53:46 +1100

    use Zenject for UMA object injection

---
+++ Assets/Scripts/TechnoTycoon/UMA/UMAInstaller.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UMA;
+using Zenject;
+
+namespace TechnoTycoon.UMA {
+		
+	public class UMAInstaller : MonoInstaller {
+		
+		public GameObject UMA;
+		//public UMAGeneratorBase generator;
+		//public SlotLibrary slotLibrary;
+		//public OverlayLibrary overlayLibrary;
+		//public RaceLibrary raceLibrary;
+		//public RuntimeAnimatorController animController;
+		
+		public override void InstallBindings() {
+			Container.Bind<OverlayLibrary>().ToSinglePrefab(UMA);
+			Container.Bind<SlotLibrary>().ToSinglePrefab(UMA);
+			Container.Bind<OverlayLibrary>().ToSinglePrefab(UMA);
+			Container.Bind<RaceLibrary>().ToSinglePrefab(UMA);
+			Container.Bind<RuntimeAnimatorController>().ToSinglePrefab(UMA);
+		}
+		
+	}
+}
